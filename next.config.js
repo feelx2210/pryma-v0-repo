77316723b1cc4be6ade87b2d0f693214a603cf82
feelx2210 <felixhuening@googlemail.com>
2025-07-28 14:09:57 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
+  basePath: "/pryma-v0-repo",
+  assetPrefix: "/pryma-v0-repo/",
 }
 
 module.exports = nextConfig
